fb9f0dc91c0069364c445d7d842dd21ec1154c7f
Irpan Kurniawan SP <irpanksp11@gmail.com>
2022-06-12 22:40:06 +0700

    submit challenge JS Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let teater1 = await promiseTheaterIXX();
+  let teater2 = await promiseTheaterVGC();
+  let gabunganTeater = [...teater1, ...teater2];
+  let result = 0;
+  gabunganTeater.map((el) => el.hasil === emosi && (result += 1));
+  return result;
+};
 
 module.exports = {
   promiseOutput,
